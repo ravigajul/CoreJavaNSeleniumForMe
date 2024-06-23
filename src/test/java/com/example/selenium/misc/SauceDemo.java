384ebd0d916bad4cdbb038020c3277b3b95d410f
ravi.gajul <ravi.gajul@yahoo.com>
2024-06-22 23:45:22 -0700

    add example to loop through elements

---
+++ src/test/java/com/example/selenium/misc/SauceDemo.java
@@ -0,0 +1,27 @@
+package com.example.selenium.misc;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+
+public class SauceDemo extends BaseTest {
+
+    @Test
+    public void addAllItemsToCart() {
+        driver.get("https://www.saucedemo.com/");
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+        List<WebElement> allBtns = driver.findElements(By.xpath("//button[text()='Add to cart']"));
+        for (WebElement btn : allBtns) {
+            btn.click();
+        }
+
+        Assert.assertEquals(driver.findElement(By.cssSelector("span.shopping_cart_badge")).getText(), "6");
+    }
+
+}
